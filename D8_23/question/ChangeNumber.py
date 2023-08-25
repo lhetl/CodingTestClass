5c06b1ab0351910774102ad47b89a3a0ab702bad
lhetl <93638355+lhetl@users.noreply.github.com>
2023-08-25 10:03:12 +0900

    :wrench: 8진수 2진수 변환 시간초과 수정

--- D8_23/question/ChangeNumber.py
+++ D8_23/question/ChangeNumber.py
@@ -1,9 +1,10 @@
-number8=input()
-if(number8[0]=='0'):
-    number8=number8[1:]
-number2=""
-for i in number8:
-    print(format(int(i),'03b'),end="")
+print(bin(int(input(),8))[2:])
+# number8=input()
+# if(number8[0]=='0'):
+#     number8=number8[1:]
+# number2=""
+# for i in number8:
+#     print(format(int(i),'03b'),end="")
     # number2 += format(int(i), '03b')
 # if(number2[0]=='0'):
 #     number2=number2[1:]
